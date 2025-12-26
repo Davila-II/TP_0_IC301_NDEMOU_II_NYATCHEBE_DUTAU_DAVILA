c57218eda908227fe97f082f468bfbf9404c6a03
Davila-II <dutaundemou@gmail.com>
2025-12-26 22:45:30 +0100

    Finalisation de l'arborescence et ajout de tous les principes SOLID

---
+++ ISP/ISP_Apres.java
@@ -0,0 +1,13 @@
+package ISP;
+
+interface Workable { void work(); }
+interface Eatable { void eat(); }
+
+class Human implements Workable, Eatable {
+    public void work() { }
+    public void eat() { }
+}
+
+class Robot implements Workable {
+    public void work() { }
+}
--- SRP/BookSRP.java
+++ SRP/BookSRP.java
@@ -2,8 +2,7 @@ public class BookSRP {
     private String title;
     private String author;
     private String text;
-    // La classe ne gère QUE les données du livre
-}
+    }
 
 class BookPrinter {
     public void printTextToConsole(String text) {
